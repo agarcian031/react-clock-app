b1a19102107957bb7678a49b77cae36a09ec9d12
Amanda Garcia <algarcian@outlook.com>
2019-06-05 10:58:27 -0600

    Add toggle and styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Container, Button, Divider, Header, Segment} from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    showClock: false,
+  }; 
+
+  toggleShowClock = () => {
+    // Will return what we want the new state to be and then refresh that state. 
+    this.setState( state => {
+      return {
+        showClock: !state.showClock, 
+      };
+    });
+  }; 
+
+  render() {
+    return (
+      <Container textAlign="center" style={{marginTop: "25px", letterSpacing: "3px", }}>
+        <Header textAlign="center" color="purple" size="huge"><i class="fas fa-clock"></i>
+        React Clock</Header>
+        <Divider/>
+        <Segment raised color="blue"> 
+          <Button onClick={this.toggleShowClock} 
+          color="purple" 
+          size="mini" 
+          inverted>
+            Toggle Clock</Button>
+        </Segment>
+      </Container>
+    )
+  }
 }
 
-export default App;
+export default App; 
+
+// Lifecycle Methods:::: 
+// Toggle Clock will have the clock mount when clicked and then unmount when not clicked. 
